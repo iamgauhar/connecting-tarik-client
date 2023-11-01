af883129bb3910ce18b4115c279578a3d53d26da
Mohammad Tariq <fk68790@gmail.com>
2023-11-01 23:17:02 +0530

    gallery click

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -39,11 +39,21 @@ function Gallery() {
             name: 'name',
         },
     ]);
+    const [selectedImage, setSelectedImage] = useState(null);
 
     const breakpointColumnsObj = {
-        default: 3, // Number of columns for default screen width
-        1100: 2, // Number of columns for screen width >= 1100px
-        700: 1, // Number of columns for screen width >= 700px
+        default: 4,
+        1100: 3,
+        700: 2,
+        450: 1,
+    };
+
+    const handleImageClick = (index) => {
+        setSelectedImage(index);
+    };
+
+    const closeImageModal = () => {
+        setSelectedImage(null);
     };
 
     return (
@@ -54,12 +64,16 @@ function Gallery() {
                 columnClassName="my-masonry-grid_column"
             >
                 {images.map((image, index) => (
-                    <div key={index} className="relative group">
+                    <div
+                        key={index}
+                        className="relative group"
+                        onClick={() => handleImageClick(index)}
+                    >
                         <div className="relative">
                             <img
                                 src={image.url}
                                 alt={image.alt}
-                                className="w-full h-auto"
+                                className="w-full h-auto cursor-pointer"
                             />
                             <div className="absolute left-5 bottom-5 w-full bg-opacity-50 text-white p-2 text-xl font-bold opacity-0 group-hover:opacity-100">
                                 {image.name}
@@ -68,6 +82,25 @@ function Gallery() {
                     </div>
                 ))}
             </Masonry>
+
+            {selectedImage !== null && (
+                <div
+                    className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50 bg-black bg-opacity-75"
+                    onClick={closeImageModal} // Close the modal when clicking outside
+                >
+                    <div className="bg-white p-4 rounded-lg h-[90%] relative">
+                        <img
+                            src={images[selectedImage].url}
+                            alt={images[selectedImage].alt}
+                            className="h-[100%] object-contain"
+                        />
+
+                        <div className="absolute left-5 bottom-5 w-full  text-white p-2 text-xl font-bold ">
+                            {images[selectedImage].name}
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
