2ee97b5257d16fb03634fce26185110016a9ae9a
iamgauhar <iamgauhar@yahoo.com>
2023-11-02 15:16:52 +0530

    finishing touch gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -57,50 +57,52 @@ function Gallery() {
     };
 
     return (
-        <div className="my-[4.4%] w-[90%] mx-auto">
-            <Masonry
-                breakpointCols={breakpointColumnsObj}
-                className="my-masonry-grid"
-                columnClassName="my-masonry-grid_column"
-            >
-                {images.map((image, index) => (
+        <div className="my-[4.4%]  pl-4 pr-2">
+            <div className='w-full pt-10 mx-auto md:w-4/5'>
+                <Masonry
+                    breakpointCols={breakpointColumnsObj}
+                    className="my-masonry-grid"
+                    columnClassName="my-masonry-grid_column"
+                >
+                    {images.map((image, index) => (
+                        <div
+                            key={index}
+                            className="relative group"
+                            onClick={() => handleImageClick(index)}
+                        >
+                            <div className="relative p-[2px] bg-black">
+                                <img
+                                    src={image.url}
+                                    alt={image.alt}
+                                    className="w-full h-auto cursor-pointer"
+                                />
+                                <div className="absolute left-5 bottom-5 w-5/6 bg-opacity-50 text-white p-2 text-xl font-bold opacity-0 group-hover:opacity-100">
+                                    {image.name}
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </Masonry>
+
+                {selectedImage !== null && (
                     <div
-                        key={index}
-                        className="relative group"
-                        onClick={() => handleImageClick(index)}
+                        className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50 bg-black bg-opacity-75"
+                        onClick={closeImageModal} // Close the modal when clicking outside
                     >
-                        <div className="relative">
+                        <div className="bg-white p-4 rounded-lg h-[90%] relative">
                             <img
-                                src={image.url}
-                                alt={image.alt}
-                                className="w-full h-auto cursor-pointer"
+                                src={images[selectedImage].url}
+                                alt={images[selectedImage].alt}
+                                className="h-[100%] object-contain"
                             />
-                            <div className="absolute left-5 bottom-5 w-full bg-opacity-50 text-white p-2 text-xl font-bold opacity-0 group-hover:opacity-100">
-                                {image.name}
+
+                            <div className="absolute left-5 bottom-5 w-full  text-white p-2 text-xl font-bold ">
+                                {images[selectedImage].name}
                             </div>
                         </div>
                     </div>
-                ))}
-            </Masonry>
-
-            {selectedImage !== null && (
-                <div
-                    className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50 bg-black bg-opacity-75"
-                    onClick={closeImageModal} // Close the modal when clicking outside
-                >
-                    <div className="bg-white p-4 rounded-lg h-[90%] relative">
-                        <img
-                            src={images[selectedImage].url}
-                            alt={images[selectedImage].alt}
-                            className="h-[100%] object-contain"
-                        />
-
-                        <div className="absolute left-5 bottom-5 w-full  text-white p-2 text-xl font-bold ">
-                            {images[selectedImage].name}
-                        </div>
-                    </div>
-                </div>
-            )}
+                )}
+            </div>
         </div>
     );
 }
